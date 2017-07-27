407be6a1f0ba0fe4c7c7111966936d9605a04f2f
Yonatan Kogan <yonatan@tockhq.com>
2017-07-26 20:44:23 -0500

    Fix lawyer number issue

--- web/src/js/state/settings.js
+++ web/src/js/state/settings.js
@@ -9,7 +9,9 @@ export default function reducer(state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
     case 'SET_SETTINGS': {
-      strings.setLanguage(payload.language);
+      if (payload.language) {
+        strings.setLanguage(payload.language);
+      }
       return {
         ...state,
         ...payload,
