09150afac7a9a000fdd6ae2f96723170385050da
Yonatan Kogan <yonatan@tockhq.com>
2017-11-05 10:23:31 -0500

    Add missing file

---
+++ web/src/js/constants/supportedLanguages.js
@@ -0,0 +1,14 @@
+import keyMirror from 'key-mirror';
+
+export const languageOptions = [{
+  label: 'EN',
+  value: 'en',
+}, {
+  label: 'ES',
+  value: 'es',
+}];
+
+export default keyMirror({
+  en: null,
+  es: null,
+});
